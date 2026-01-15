8811f366e1b36574cec6e9f734c7286fdd6f4e00
AakashShahi <shahiaakash112@gmail.com>
2026-01-15 16:02:49 +0545

    feat: optimize socket connection

--- WorkDay_Web/Workday/src/utils/socket.js
+++ WorkDay_Web/Workday/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:5050", {
+const socket = io(import.meta.env.VITE_BACKEND_URL || "https://localhost:5050", {
 });
 
 export default socket;
