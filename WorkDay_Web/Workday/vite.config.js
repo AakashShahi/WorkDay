8a7528f0132a61d5435cfec92fee528f2f970a03
AakashShahi <shahiaakash112@gmail.com>
2026-01-15 16:02:51 +0545

    chore: secure production build config

--- WorkDay_Web/Workday/vite.config.js
+++ WorkDay_Web/Workday/vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import fs from 'fs'
+import path from 'path'
+// import obfuscator from 'vite-plugin-obfuscator'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '../../certs/server.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../../certs/server.crt')),
+    },
+    port: 5173,
+  },
+  build: {
+    minify: 'esbuild',
+    sourcemap: false, // Security: Don't ship sourcemaps to production
+    reportCompressedSize: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
 })
